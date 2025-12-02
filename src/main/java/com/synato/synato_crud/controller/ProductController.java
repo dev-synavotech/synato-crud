60ee54d5fc2ac56c7ecb80e7a402d8c8f3f8c966
nellorukiran <nellorukiran@gmail.com>
2025-12-02 09:45:46 +0530

    Added CRUD operation

---
+++ src/main/java/com/synato/synato_crud/controller/ProductController.java
@@ -0,0 +1,47 @@
+package com.synato.synato_crud.controller;
+
+import com.synato.synato_crud.entity.Product;
+import com.synato.synato_crud.service.ProductService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.*;
+
+import java.util.List;
+
+@RestController
+@RequestMapping("/products")
+public class ProductController {
+
+    @Autowired
+    private ProductService productService;
+
+    @PostMapping
+    public ResponseEntity<Product> createProduct(@RequestBody Product product) {
+        Product createdProduct = productService.createProduct(product);
+        return new ResponseEntity<>(createdProduct, HttpStatus.CREATED);
+    }
+
+    @GetMapping
+    public List<Product> getAllProducts() {
+        return productService.getAllProducts();
+    }
+
+    @GetMapping("/{id}")
+    public ResponseEntity<Product> getProductById(@PathVariable Long id) {
+        Product product = productService.getProductById(id);
+        return ResponseEntity.ok(product);
+    }
+
+    @PutMapping("/{id}")
+    public ResponseEntity<Product> updateProduct(@PathVariable Long id, @RequestBody Product productDetails) {
+        Product updatedProduct = productService.updateProduct(id, productDetails);
+        return ResponseEntity.ok(updatedProduct);
+    }
+
+    @DeleteMapping("/{id}")
+    public ResponseEntity<Void> deleteProduct(@PathVariable Long id) {
+        productService.deleteProduct(id);
+        return ResponseEntity.noContent().build();
+    }
+}
---
+++ src/test/java/com/synato/synato_crud/SynatoCrudApplicationTests.java
@@ -0,0 +1,13 @@
+package com.synato.synato_crud;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SynatoCrudApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
